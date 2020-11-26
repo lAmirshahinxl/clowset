c38d57d92e6db794c8ce5d7cf77335aea309ccd5
mobiniqow <mobiniqow@gmail.com>
2020-11-26 09:57:36 +0330

    splash

---
+++ lib/pages/SabadKharid.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class SabadKharid extends StatefulWidget {
+  @override
+  _SabadKharidState createState() => _SabadKharidState();
+}
+
+class _SabadKharidState extends State<SabadKharid> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("SabadKharid"),);
+  }
+}
